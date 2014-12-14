83f3eb7362a217f14f835521523f4ba9bb01b34f
Ivan Voroshilin <vibneiro@gmail.com>
2014-12-14 20:02:15 +0300

    Scala/Akka Tutorial 1
    Concurrent Akka calculator of Pi number

---
+++ PiCalculator/src/main/scala/Messages.scala
@@ -0,0 +1,10 @@
+import akka.util.Duration
+
+object Messages {
+
+  sealed trait PiMessage
+  case object Calculate extends PiMessage
+  case class Work(start: Int, nrOfElements: Int) extends PiMessage
+  case class Result(value: Double) extends PiMessage
+  case class PiApproximation(pi: Double, duration: Duration)
+}
---
+++ PiCalculator/src/main/scala/Worker.scala
@@ -0,0 +1,19 @@
+import Messages.{Result, Work}
+import akka.actor.Actor
+
+class Worker extends Actor {
+
+
+  def receive = {
+    case Work(start, nrOfElements) =>
+      sender ! Result(calculatePiFor(start, nrOfElements)) // perform the work
+  }
+
+  def calculatePiFor(start: Int, nrOfElements: Int): Double = {
+    var acc = 0.0
+    for (i ← start until (start + nrOfElements))
+      acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
+    acc
+  }
+
+}
